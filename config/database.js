7c0bd66b846d05939a8d77327d97057fdf901027
tiankay999 <tiankay999@gmail.com>
2025-11-26 16:22:20 +0000

    database.config bug solved

--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
-// config/database.js (example)
- const dotenv= require('dotenv')
- dotenv.config();  
+// config/database.js
+const dotenv = require("dotenv");
+dotenv.config();
+
 const { Sequelize } = require("sequelize");
-const mysql2 = require("mysql2"); // 👈 force-load mysql2
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,        
-  process.env.DB_USER,        
-  process.env.DB_PASSWORD || '',   
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || 'localhost', // ⚠️ must be a cloud DB host, NOT localhost, when on Vercel
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
-    dialectModule: mysql2,     // 👈 tell Sequelize to use this module
+    dialectModule: mysql2,
     logging: false,
   }
 );
