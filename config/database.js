bae19ab67b58e36a5268c87880f83b3223f49fe1
tiankay999 <tiankay999@gmail.com>
2025-12-08 16:10:38 +0000

    update the database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: false,  // For local / Render MySQL NO SSL
+    },
   }
-});
+);
 
 module.exports = { sequelize };
